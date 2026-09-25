cee0b814b00d07502e5f497d7cc4f8072ddff169
clauubueyes <clauubueyes@outlook.com>
2026-09-25 18:47:13 +0200

    feat: parse birth date from form input

--- 01-fundamentals/exercises/web/script.js
+++ 01-fundamentals/exercises/web/script.js
@@ -2,12 +2,27 @@ const inputName = document.getElementById("fname");
 const inputBirthdate= document.getElementById("birthdate");
 const dataForm = document.getElementById("ageCalculatorForm");
 
+
 dataForm.addEventListener("submit", function(event){
     event.preventDefault();
 
-    const name = inputName.value; 
-    const birthdate = inputBirthdate.value;
+    const currentDate = new Date();
 
-    console.log(name);
+    const currentYear = currentDate.getFullYear();
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth() + 1;
+
+    const birthdate = new Date(inputBirthdate.value);
+
+    const birthYear = birthdate.getFullYear() ; 
+    const birthDay = birthdate.getDate();
+    const birthMonth = birthdate.getMonth() + 1;
+
+    console.log(birthYear);
+    console.log(birthMonth);
+    console.log(birthDay);
     console.log(birthdate);
+    
+    const name = inputName.value; 
+    console.log(name);
 });
