0743739fc37e26e2583f70d1076b9cd20c762bae
clauubueyes <clauubueyes@outlook.com>
2026-09-25 17:50:56 +0200

    feat: handle age calculator form submission

--- 01-fundamentals/exercises/web/script.js
+++ 01-fundamentals/exercises/web/script.js
@@ -1,5 +1,12 @@
 const inputName = document.getElementById("fname");
 const inputBirthdate= document.getElementById("birthdate");
+const dataForm = document.getElementById("ageCalculatorForm");
 
 console.log(inputName);
 console.log(inputBirthdate);
+console.log(dataForm);
+
+dataForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log("Funcion correcta");
+});
