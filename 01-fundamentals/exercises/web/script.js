0fcf6010c30bafffd8cbfd2e3fa0559e545a07aa
clauubueyes <clauubueyes@outlook.com>
2026-09-25 18:05:12 +0200

    feat: read age calculator form values

--- 01-fundamentals/exercises/web/script.js
+++ 01-fundamentals/exercises/web/script.js
@@ -2,11 +2,12 @@ const inputName = document.getElementById("fname");
 const inputBirthdate= document.getElementById("birthdate");
 const dataForm = document.getElementById("ageCalculatorForm");
 
-console.log(inputName);
-console.log(inputBirthdate);
-console.log(dataForm);
-
 dataForm.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log("Funcion correcta");
+
+    const name = inputName.value; 
+    const birthdate = inputBirthdate.value;
+
+    console.log(name);
+    console.log(birthdate);
 });
