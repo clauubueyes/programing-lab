0ccae370f88a84c99a6dd769aabbeaab13f3089f
clauubueyes <clauubueyes@outlook.com>
2026-09-26 17:00:32 +0200

    refactor: restauring age calculator exercice

---
+++ 01-fundamentals/exercises/001-age-calculator/web/script.js
@@ -0,0 +1,27 @@
+const inputName = document.getElementById("fname");
+const inputBirthdate= document.getElementById("birthdate");
+const dataForm = document.getElementById("ageCalculatorForm");
+const ageResult = document.getElementById("age");
+
+dataForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const currentDate = new Date();
+
+    const currentYear = currentDate.getFullYear();
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth() + 1;
+
+    const birthdate = new Date(inputBirthdate.value);
+
+    const birthYear = birthdate.getFullYear() ; 
+    const birthDay = birthdate.getDate();
+    const birthMonth = birthdate.getMonth() + 1;
+
+    const name = inputName.value; 
+    let age = currentYear - birthYear;
+    if( (currentMonth < birthMonth) || ((currentMonth === birthMonth) && (currentDay < birthDay))){
+        age = age-1;
+    }
+    ageResult.textContent = `Hello ${name}! You are ${age} years old.`;
+});
